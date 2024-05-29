337b8ef095e86a74f4fdb512e3828ff3a219bafb
panjiajie <panjiajie@chexiao.co>
2024-05-29 14:14:04 +0800

    position change

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import controller from './controller';
 
-const controllers = Object.values(controller);
-console.log(controllers);
 @Module({
   imports: [],
-  controllers: [AppController, ...controllers],
+  controllers: Object.values(controller),
   providers: [AppService],
 })
 export class AppModule {}
--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,6 +1,8 @@
 // controller收拢文件
+import { AppController } from './app.controller';
 import { UserController } from './user.controller';
 
 export default {
+  AppController,
   UserController,
 };
