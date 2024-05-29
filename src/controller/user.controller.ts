f5f4e0bd03c6791db8fa04c14fa36bdd3e1fc597
panjiajie <panjiajie@chexiao.co>
2024-05-29 14:38:57 +0800

    service

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get, Req } from '@nestjs/common';
 import { Request } from 'express';
-
+import { UserService } from '../service/user.service';
 @Controller('user')
 export class UserController {
+  constructor(private useService: UserService) {}
+
   @Get()
   getUsers(@Req() request: Request): string {
     console.info(request);
--- src/service/index.ts
+++ src/service/index.ts
@@ -1,5 +1,7 @@
 import { AppService } from './app.service';
+import { UserService } from './user.service';
 
 export default {
   AppService,
+  UserService,
 };
