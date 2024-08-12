d08996c861870208ed615008eaadb4a05bc3f1c1
panjiajie <panjiajie@chexiao.co>
2024-08-12 10:24:52 +0800

    user

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get, HttpStatus } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Req } from '@nestjs/common';
 import { UserService } from '../service/user.service';
 @Controller('user')
 export class UserController {
   constructor(private useService: UserService) {}
 
   @Get()
-  async getUsers(): Promise<string> {
+  async getUsers(@Req() request: Request): Promise<string> {
     const users = await this.useService.getUsers();
     if (!users) {
       return '' + HttpStatus.NOT_FOUND;
