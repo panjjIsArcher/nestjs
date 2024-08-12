5814f24c3e5292526d3033f255506ec2b24a0eb2
panjiajie <panjiajie@chexiao.co>
2024-08-12 19:48:06 +0800

    entity

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,16 +1,17 @@
 import { Controller, Get, HttpStatus, Req } from '@nestjs/common';
 import { UserService } from '../service/user.service';
 import { User } from 'src/interfaces/user.interface';
+import { ResponseDto } from 'src/dto/response.dto';
 @Controller('user')
 export class UserController {
   constructor(private useService: UserService) {}
 
   @Get()
-  async getUsers(@Req() request: Request): Promise<User[]> {
+  async getUsers(@Req() request: Request): Promise<ResponseDto<User[]>> {
     const users = await this.useService.getUsers();
     if (!users) {
-      return [];
+      return new ResponseDto(0, [], 'success');
     }
-    return users;
+    return new ResponseDto(0, users, 'success');
   }
 }
