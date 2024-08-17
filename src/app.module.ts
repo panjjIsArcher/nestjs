a3c7b8312a41aa1859007372801c3219d395501f
panjiajie <panjiajie@chexiao.co>
2024-08-17 18:19:05 +0800

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import controller from './controller';
 import service from './service';
 @Module({
+  providers: Object.values(service),
   imports: [],
   controllers: Object.values(controller),
-  providers: Object.values(service),
 })
 export class AppModule {}
